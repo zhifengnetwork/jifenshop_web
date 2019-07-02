f2db04f7c663f06ee360ffbadc56977f26e00b17
chenfeixin <18825481575@163.com>
2019-07-02 14:59:50 +0800

    haha

--- src/pages/user/address/SelectPoint.vue
+++ src/pages/user/address/SelectPoint.vue
@@ -16,10 +16,12 @@ export default {
     name:'selectPoint',
     created: function(){
         var that = this;
+        // console.log(event)
         window.addEventListener('message', function(event){
             // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
             var loc = event.data;
             if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
+                console.log(event.data);
                 // console.log('location', loc.poiaddress,loc.poiname);
                 // console.log('location', loc);
                 // 跳转路由
@@ -28,6 +30,9 @@ export default {
             }
         }, false);
     },
+    mounted: function () {
+        console.log(event);
+    }
 }
 </script>
 
