9210bbce61d8b2841e91bc90a88fe13c0947519f
ZZZhangYunfei <86975943@qq.com>
2019-07-05 18:49:51 +0800

    00000

--- src/App.vue
+++ src/App.vue
@@ -8,112 +8,53 @@
 export default {
   name: "App",
   mounted() {
-    //判断是否为微信环境
-	var ua = navigator.userAgent.toLowerCase();
-	alert(ua)
-
+    //判断是否为微信环境 --- 两行代码
+    var ua = navigator.userAgent.toLowerCase();
     if (ua.match(/MicroMessenger/i) == "micromessenger") {
-
-		alert("micromessenger")
-
-		var code = window.location.href.indexOf('code');
-		alert(code)
-		if(code == null || code === '' || code == undefined){
-			var local = window.location.href // 获取页面url
-			this.$axios({
-					method: "post",
-					url: "/login/get_code_url?baseUrl=" + local
-				}).then(res => {
-					console.log(res.data.data);
-					var lianjie = res.data.data;
-					window.location.href = local;
-				});
-
-		}else{
-			alert("ssss")
-		}
-
-
-		//   if (1 == 1) {
-		//     console.log("无token,有code");
-		//   } else {
-		//     console.log("无token,无code");
-		//   }
-		
-			
-		
-
-
-		} else {
-
-		alert('不是微信环境');
-       
-	
-		
-		// if (this.code == null || this.code === '') { // 如果没有code，则去请求
-		// 	// window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${encodeURIComponent(local)}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
-				
-
-		// } else {
-		// 	// 你自己的业务逻辑
-		// }
-     
-     
-	}
-	//  if(!window.localStorage.getItem('token')){ // 如果缓存localStorage中没有微信openId，则需用code去后台获取
-    //         this.getCode()
-    //     } else {
-    //         // 别的业务逻辑
-    //     }
-
-
-
-
-
-
-
-
-
+	//  在本地获取token,如果token为空或者为null或者为null的时候,就要请求后台,获取token	
+      var huoqutoken = window.sessionStorage.getItem("token");
+      if (huoqutoken == "" || huoqutoken == null || huoqutoken == undefined) {
+        if ( window.location.href.indexOf("code") == 0 ||window.location.href.indexOf("code") <= 0) {
+          var local = window.location.href; // 获取页面url
+          this.$axios({
+            method: "get",
+            url: "/login/get_code_url?baseUrl=" + local
+          }).then(res => {
+            console.log(res.data.data);
+            var lianjie = res.data.data;
+            window.location.href = lianjie;
+          });
+        } else {
+          var code = this.getQueryString("code");
+          // alert(code)
+          this.$axios({
+            method: "get",
+            url: "/login/login_by_code?code=" + code
+          }).then(res => {
+            // alert(res.data.data.token)
+            //获取token,储存到本地
+            if (res.data.status == 1) {
+              window.sessionStorage.setItem("token", res.data.data.token);
+            }
+          });
+        }
+      }
+    } else {
+		 window.sessionStorage.setItem("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEQyIsImlhdCI6MTU1OTYzOTg3MCwiZXhwIjoxNTU5Njc1ODcwLCJ1c2VyX2lkIjo3Nn0.YUQ3hG3TiXzz_5U594tLOyGYUzAwfzgDD8jZFY9n1WA");
+    }
   },
-  methods:{
-	  getCode () { // 非静默授权，第一次有弹框
-                // this.code = ''
-                // var local = window.location.href // 获取页面url
-                // // var appid = 'wx65adcf075369****' 
-				// this.code = this.getUrlCode().code // 截取code
-				// console.log("dsssssssss")
-				// console.log(this.code)
-				// console.log("dsssssssss")
-                // if (this.code == null || this.code === '') { // 如果没有code，则去请求
-				// 	// window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${encodeURIComponent(local)}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
-				// 	 this.$axios({
-				// 			method: "post",
-				// 			url: "/login/get_code_url?baseUrl=" + local
-				// 		}).then(res => {
-				// 			console.log(res.data.data);
-				// 			var lianjie = res.data.data;
-				// 			window.location.href = url;
-				// 		});
-
-                // } else {
-                //     // 你自己的业务逻辑
-                // }
-            },
-	//  getUrlCode() { // 截取url中的code方法
-	// 				var url = location.search
-	// 				this.winUrl = url
-	// 				var theRequest = new Object()
-	// 				if (url.indexOf("?") != -1) {
-	// 					var str = url.substr(1)
-	// 					var strs = str.split("&")
-	// 					for(var i = 0; i < strs.length; i ++) {
-	// 						theRequest[strs[i].split("=")[0]]=(strs[i].split("=")[1])
-	// 					}
-	// 				}
-	// 				return theRequest
-	// 			}
-
-  }}
+  methods: {
+    getQueryString(name) {
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+      var r = window.location.search.substr(1).match(reg);
+      if (r != null) {
+        return unescape(r[2]);
+      } else {
+        return null;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="stylus">
