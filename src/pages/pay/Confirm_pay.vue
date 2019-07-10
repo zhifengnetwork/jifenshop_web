f72604541e5903d4c881b08bb9cfe2d2f9bae91c
ICEY-I-SEE <1141839113@qq.com>
2019-07-10 15:13:07 +0800

    00

--- src/pages/pay/Confirm_pay.vue
+++ src/pages/pay/Confirm_pay.vue
@@ -172,9 +172,9 @@ export default {
             function(res){
                 // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
                 if(res.err_msg == "get_brand_wcpay_request:ok" ){         
-                    Toast.success('成功111');
+                    Toast.success('支付成功');
                 }else{           
-                    Toast('失败222');
+                    Toast('支付失败');
                 }
             }
             );
